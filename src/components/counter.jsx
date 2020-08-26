438a836cabf5d1e9f4d780cc9d6cd5157b16acb9
MadhuGautam <58028102+MadhuGautam@users.noreply.github.com>
2020-08-26 11:26:27 +0530

    embed boostrap and css style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,13 +5,24 @@ class Counter extends Component {
     count: 0,
     imageUrl: "https://picsum.photos/200",
   };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
   render() {
     return (
       <React.Fragment>
         <img src={this.state.imageUrl} alt="" />
-        <span>{this.state.count}</span>
-        <span>{this.formatCount()}</span>
-        <button>Increement</button>
+        <br />
+
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.state.count}
+        </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm m-5">Increement</button>
       </React.Fragment>
     );
   }
